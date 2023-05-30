9c4ad56848bc1af8762c95002fb82dbc29d2c6f4
Anvyar <anvyar.narbikov@ya.ru>
2023-05-30 15:41:00 +0300

    Onboarding finished

--- app/src/main/java/com/skillcinema/ui/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/skillcinema/ui/onboarding/OnboardingFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
@@ -32,7 +31,6 @@ class OnboardingFragment : Fragment() {
         viewPager.adapter = adapter
 
         binding.onBoardingTextViewSkip.setOnClickListener{
-            Toast.makeText(requireContext(),"HHGTDHTHT",Toast.LENGTH_SHORT).show()
             findNavController().popBackStack()
         }
     }
