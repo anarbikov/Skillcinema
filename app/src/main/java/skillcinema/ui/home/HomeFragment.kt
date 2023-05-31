9c5d73ca76854689abe9a7fd913a51bce374f24c
Anvyar <anvyar.narbikov@ya.ru>
2023-05-31 16:16:14 +0300

    Sharedprefs for onboarding added

--- app/src/main/java/skillcinema/ui/home/HomeFragment.kt
+++ app/src/main/java/skillcinema/ui/home/HomeFragment.kt
@@ -1,5 +1,7 @@
-package com.ui.home
+package skillcinema.ui.home
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,13 +13,13 @@ import androidx.navigation.fragment.findNavController
 import com.skillcinema.R
 import com.skillcinema.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
-import skillcinema.ui.home.HomeViewModel
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
+    private lateinit var prefs: SharedPreferences
+    private var onboardingShown: Int = 0
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -42,11 +44,30 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        findNavController().navigate(R.id.action_navigation_home_to_numberFragment)
+        onboardingShown = getDataFromSharedPreference()
+        if (onboardingShown ==0) {
+            saveOnboardingFlag()
+            findNavController().navigate(R.id.action_navigation_home_to_numberFragment)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    private fun getDataFromSharedPreference(): Int {
+        prefs = requireContext().getSharedPreferences(PREFERENCE_NAME,Context.MODE_PRIVATE)
+        return prefs.getInt(KEY_INT_NAME, 0)
+    }
+    private fun saveOnboardingFlag(){
+        onboardingShown = 1
+        prefs = requireContext().getSharedPreferences(PREFERENCE_NAME,Context.MODE_PRIVATE)
+        val editor:SharedPreferences.Editor = prefs.edit()
+        editor.putInt(KEY_INT_NAME,1)
+        editor.apply()
+    }
+    companion object{
+        private  const val PREFERENCE_NAME = "prefs_name"
+        private const val KEY_INT_NAME = "KEY_STRING"
     }
 }
