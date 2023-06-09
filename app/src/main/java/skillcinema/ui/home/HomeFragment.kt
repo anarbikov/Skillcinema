d83c213bd137ea6e325028013783cdfa6ee7600c
Anvyar <anvyar.narbikov@ya.ru>
2023-06-09 12:49:39 +0300

    Fixed RecyclerView - show all works correctly

--- app/src/main/java/skillcinema/ui/home/HomeFragment.kt
+++ app/src/main/java/skillcinema/ui/home/HomeFragment.kt
@@ -5,11 +5,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.OnScrollListener
 import com.skillcinema.R
@@ -44,30 +44,32 @@ class HomeFragment : Fragment() {
         if (viewModel.onboardingShownFlag == 0) findNavController().navigate(R.id.action_navigation_home_to_numberFragment)
         setPremieresRecyclerView()
 
-
     }
 
     private fun onItemClick(item: Film) {
-        val url = bundleOf("url" to item.posterUrlPreview)
+//        val item = bundleOf("item" to item)
         Log.d("mytag","clicked")
     }
 
     private fun setPremieresRecyclerView() {
         binding.premiereRecyclerView.adapter = pagedPremiereAdapter
         binding.premiereRecyclerView.addItemDecoration(RecyclerItemDecoration(21, 8, false))
+        binding.homeTextViewPremieresAll.setOnClickListener{Log.d("mytag","clicked")}
         viewModel.pagedPremiere.onEach {
             pagedPremiereAdapter.submitData(it)
         }.launchIn(viewLifecycleOwner.lifecycleScope)
         binding.premiereRecyclerView.addOnScrollListener(object : OnScrollListener() {
-            var count = 0
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
-                count += dx
-                binding.premiereShowAllLayout.visibility =
-                    if (count > 5437) View.VISIBLE else View.GONE
+                val layoutManager = LinearLayoutManager::class.java.cast(recyclerView.layoutManager)
+                val itemCount = recyclerView.layoutManager?.itemCount
+                val lastVisible = layoutManager!!.findLastVisibleItemPosition()
+                if (itemCount == lastVisible+1) binding.premiereShowAllLayout.visibility = View.VISIBLE
+                else binding.premiereShowAllLayout.visibility = View.GONE
             }
         })
         binding.premiereRecyclerView.setOnClickListener { findNavController().navigate(R.id.action_navigation_home_to_numberFragment) }
+        binding.premiereRecyclerView.scrollToPosition(15)
     }
 
     override fun onDestroyView() {
