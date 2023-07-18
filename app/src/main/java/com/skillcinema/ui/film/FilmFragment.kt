bb0fb682ced547010ffd1f14e6b5d79e104a09c1
Anvyar <anvyar.narbikov@ya.ru>
2023-07-18 10:38:59 +0300

    filmfragment used concatadapter

--- app/src/main/java/com/skillcinema/ui/film/FilmFragment.kt
+++ app/src/main/java/com/skillcinema/ui/film/FilmFragment.kt
@@ -11,8 +11,8 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import com.bumptech.glide.Glide
-import com.skillcinema.R
+import androidx.recyclerview.widget.ConcatAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.skillcinema.databinding.FragmentFilmBinding
 import com.skillcinema.entity.FilmInfo
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,6 +27,8 @@ class FilmFragment : Fragment() {
     private var _binding: FragmentFilmBinding? = null
     private val binding get() = _binding!!
     private val viewModel: FilmViewModel by viewModels()
+    private lateinit var generalInfoAdapter: FilmGeneralInfoAdapter
+    private lateinit var concatAdapter: ConcatAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,7 +46,12 @@ class FilmFragment : Fragment() {
         setUpViews()
         doObserveWork(kinopoiskId)
     }
-    private fun setUpViews(){}
+    private fun setUpViews(){
+        generalInfoAdapter = FilmGeneralInfoAdapter(requireContext())
+        concatAdapter = ConcatAdapter(generalInfoAdapter)
+        binding.concatRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.concatRecyclerView.adapter = concatAdapter
+    }
     private fun doObserveWork(kinopoiskId:Int){
         viewModel.loadAll(kinopoiskId)
         viewLifecycleOwner.lifecycleScope.launch {
@@ -52,13 +59,13 @@ class FilmFragment : Fragment() {
                 viewModel.isLoading.collect {
                     when (it) {
                         true -> {
-                            binding.wholeScreen.visibility = View.GONE
+                            binding.concatRecyclerView.visibility = View.GONE
                             binding.loadingProgress.visibility = View.VISIBLE
                             requireActivity().nav_view.visibility = View.GONE
                         }
 
                         else -> {
-                            binding.wholeScreen.visibility = View.VISIBLE
+                            binding.concatRecyclerView.visibility = View.VISIBLE
                             binding.loadingProgress.visibility = View.GONE
                             requireActivity().nav_view.visibility = View.VISIBLE
                         }
@@ -75,24 +82,28 @@ class FilmFragment : Fragment() {
         if (allInfo.isEmpty())return
         Log.d("mytag",allInfo.toString())
         val generalInfo: FilmInfo = allInfo[0] as FilmInfo
-        Glide.with(requireView())
-            .load(generalInfo.posterUrlPreview)
-            .centerCrop()
-            .into(binding.posterImageView)
-        val rating = generalInfo.ratingKinopoisk?:""
-        val filmName = generalInfo.nameRu ?: (generalInfo.nameOriginal?:"")
-        val filmYear = generalInfo.year?:""
-        val filmGenre = if (generalInfo.genres.isNotEmpty() && generalInfo.genres.size == 1 )", " + generalInfo.genres[0].genre
-        else (", " + generalInfo.genres[0].genre + ", " + generalInfo.genres[1].genre)
-        val filmCountries = if (generalInfo.countries.isNotEmpty() && generalInfo.countries.size == 1 ) generalInfo.countries[0].country
-        else generalInfo.countries[0].country+", "+generalInfo.countries[1].country
-        val durationHours = if(generalInfo.filmLength != null) (generalInfo.filmLength /60) else 0
-        val durationMinutes = if (generalInfo.filmLength != null) generalInfo.filmLength - durationHours * 60 else 0
-        val duration = if (generalInfo.filmLength != null) ", $durationHours ${getString(R.string.hour)} $durationMinutes ${getString(R.string.minutes)}" else ""
-        val ageRestriction =  if (generalInfo.ratingAgeLimits!=null) ", "+ generalInfo.ratingAgeLimits.toString().drop(3)+"+" else ""
-        binding.filmNameTextView.text = "$rating $filmName\n$filmYear$filmGenre\n$filmCountries$duration$ageRestriction"
-        binding.filmDescriptionHeaderTextView.text = if (generalInfo.shortDescription != null) generalInfo.shortDescription.toString() else ""
-        binding.filmDescriptionBodyTextView.text = if (generalInfo.description != null) generalInfo.description.toString() else ""
+        generalInfoAdapter.addData(generalInfo)
+
+
+
+//        Glide.with(requireView())
+//            .load(generalInfo.posterUrlPreview)
+//            .centerCrop()
+//            .into(binding.posterImageView)
+//        val rating = generalInfo.ratingKinopoisk?:""
+//        val filmName = generalInfo.nameRu ?: (generalInfo.nameOriginal?:"")
+//        val filmYear = generalInfo.year?:""
+//        val filmGenre = if (generalInfo.genres.isNotEmpty() && generalInfo.genres.size == 1 )", " + generalInfo.genres[0].genre
+//        else (", " + generalInfo.genres[0].genre + ", " + generalInfo.genres[1].genre)
+//        val filmCountries = if (generalInfo.countries.isNotEmpty() && generalInfo.countries.size == 1 ) generalInfo.countries[0].country
+//        else generalInfo.countries[0].country+", "+generalInfo.countries[1].country
+//        val durationHours = if(generalInfo.filmLength != null) (generalInfo.filmLength /60) else 0
+//        val durationMinutes = if (generalInfo.filmLength != null) generalInfo.filmLength - durationHours * 60 else 0
+//        val duration = if (generalInfo.filmLength != null) ", $durationHours ${getString(R.string.hour)} $durationMinutes ${getString(R.string.minutes)}" else ""
+//        val ageRestriction =  if (generalInfo.ratingAgeLimits!=null) ", "+ generalInfo.ratingAgeLimits.toString().drop(3)+"+" else ""
+//        binding.filmNameTextView.text = "$rating $filmName\n$filmYear$filmGenre\n$filmCountries$duration$ageRestriction"
+//        binding.filmDescriptionHeaderTextView.text = if (generalInfo.shortDescription != null) generalInfo.shortDescription.toString() else ""
+//        binding.filmDescriptionBodyTextView.text = if (generalInfo.description != null) generalInfo.description.toString() else ""
     }
     override fun onDestroyView() {
         super.onDestroyView()
---
+++ app/src/main/java/com/skillcinema/ui/film/FilmGeneralInfoAdapter.kt
@@ -0,0 +1,67 @@
+package com.skillcinema.ui.film
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.skillcinema.R
+import com.skillcinema.databinding.FilmGeneralInfoViewBinding
+import com.skillcinema.entity.FilmInfo
+import javax.inject.Inject
+
+class FilmGeneralInfoAdapter @Inject constructor(
+//    filmData: FilmInfo,
+    val context: Context
+) : RecyclerView.Adapter<FilmGeneralInfoAdapter.ViewHolder>() {
+    inner class ViewHolder(val binding: FilmGeneralInfoViewBinding) :
+        RecyclerView.ViewHolder(binding.root) {}
+    private lateinit var filmInfo: FilmInfo
+    init {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            FilmGeneralInfoViewBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.apply {
+            Glide.with(holder.itemView.context)
+                .load( filmInfo.posterUrlPreview)
+                .centerCrop()
+                .into(this.posterImageView)
+            val rating = filmInfo.ratingKinopoisk?:""
+            val filmName = filmInfo.nameRu ?: (filmInfo.nameOriginal?:"")
+            val filmYear = filmInfo.year?:""
+            val filmGenre = if (filmInfo.genres.isNotEmpty() && filmInfo.genres.size == 1 )", " + filmInfo.genres[0].genre
+            else (", " + filmInfo.genres[0].genre + ", " + filmInfo.genres[1].genre)
+            val filmCountries = if (filmInfo.countries.isNotEmpty() && filmInfo.countries.size == 1 ) filmInfo.countries[0].country
+            else filmInfo.countries[0].country+", "+filmInfo.countries[1].country
+            val durationHours = if(filmInfo.filmLength != null) (filmInfo.filmLength!! /60) else 0
+            val durationMinutes = if (filmInfo.filmLength != null) filmInfo.filmLength!! - durationHours * 60 else 0
+            val duration = if (filmInfo.filmLength != null) ", $durationHours ${context.getString(R.string.hour)} $durationMinutes ${context.getString(
+                R.string.minutes)}" else ""
+            val ageRestriction =  if (filmInfo.ratingAgeLimits!=null) ", "+ filmInfo.ratingAgeLimits.toString().drop(3)+"+" else ""
+            this.filmNameTextView.text = "$rating $filmName\n$filmYear$filmGenre\n$filmCountries$duration$ageRestriction"
+            this.filmDescriptionHeaderTextView.text = if (filmInfo.shortDescription != null) filmInfo.shortDescription.toString() else ""
+            this.filmDescriptionBodyTextView.text = if (filmInfo.description != null) filmInfo.description.toString() else ""
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return 1
+    }
+    @SuppressLint("NotifyDataSetChanged")
+    fun addData(info: FilmInfo) {
+        this.filmInfo = info
+        notifyDataSetChanged()
+    }
+}
